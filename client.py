bb0729784ecd9d1d62213782ffe5f8b7b932a772
Pouya Rahimpour <mpouya.rhm9981@gmail.com>
2024-01-04 20:13:22 +0330

    modify client.py and server.py

--- client.py
+++ client.py
@@ -1,51 +1,66 @@
 import socket
+import subprocess
+import shlex
 
-def main():
-    host = "localhost"
-    server = "localhost"
-    control_port = 21
-    data_port = 20
+host = "localhost"
+server = "localhost"
+control_port = 8021
+data_port = 8020
 
-
-    is_online = True
+def send(request):
     try:
         client_ctrl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_ctrl.settimeout(1)
         client_ctrl.connect((server, control_port))
+        client_ctrl.sendall(request.encode())
+        response = client_ctrl.recv(1024).decode()
+        client_ctrl.close()
+
     except:
         response = "server is offline!!"
-        is_online = False
 
-    while is_online:
+    return response
+    
+def main():
+
+
+
+    while True:
         request = input().strip()
-        try:
-            client_ctrl.sendall(request.encode())
-            response = client_ctrl.recv(1024).decode()
-
-        except:
-            response = "server is offline!!"
-            is_online = False
+        response = send(request)
+        if response == "server is offline!!":
+            print(response)
             break
 
-        if "RETR" in request.to_upper():
+
+        if "RETR" in request.upper():
             client_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_data.bind((host, data_port))
             client_data.listen(1)
-            while True:
-                server_socket, server_address = client_data.accept()
-                server_host = socket.gethostbyaddr(client_address[0])[0]
-                request = client_socket.recv(1024).decode()
-                client_socket.sendall(response.encode())
-                client_socket.close()
+            file_name = request.strip().split()[1].split("/")[-1]
+            command = 'mkdir ./download/' 
+            response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
+            command = 'touch ' + file_name
+            response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
+            with open(f'./download/{file_name}', 'ab') as f:
+                while True:
+                    server_socket, server_address = client_data.accept()
+                    server_host = socket.gethostbyaddr(server_address[0])[0]
+                    data = server_socket.recv(1024).decode()
+                    f.write(data)
+                    
+                    #TODO when is file finished?
+                    server_socket.sendall(response.encode())
+                    server_socket.close()
+                    break
 
 
-        elif "STOR" in request.to_upper():
+        elif "STOR" in request.upper():
             pass
 
         
         print(response)
     
-    client.close()
 
 
 
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import subprocess
 import shlex
 #commands
 
+BAD_REQUEST = '400 Bad Request'
 ORDINARY = 3
 PRIVILAGED = 2
 ADMIN = 1
@@ -11,7 +12,7 @@ users = []
 online_users = []
 class User:
     def __init__(self):
-        pass
+        self.authorized = False
 
     def set_username(self, username):
         self.username = username
@@ -33,33 +34,44 @@ class User:
             if self.username == user.username and self.password == user.password:
                 self.set_privilage(user.privilage)
                 online_users.append(self)
-                return True
-
-        return False
+                self.authorized = True
+                break
 
     def quit(self):
         online_users.remove(self)
+    
+    def __str__(self):
+        return f'username: {self.username}\npassword: {self.password}\nprivilage: {self.privilage}\nautorization: {self.authorized}'
+
         
 
 def add_fake_users():
     user = User()
-    user.add_user('mohammad', 1234, ADMIN)
+    user.add_user('mohammad', '1234', ADMIN)
     user = User()
-    user.add_user('pouya', 123456, ORDINARY) 
+    user.add_user('p', '1', ORDINARY) 
 
 def handle_user(request, user):
     request_parts = request.strip().split()
-    username = request_parts[1]
+    try:
+        username = request_parts[1]
+    except:
+        return BAD_REQUEST
+
     user.set_username(username)
     response = 'Username Set'
     return response
 
 def handle_pass(request, user):
     request_parts = request.strip().split()
-    password = request_parts[1]
+    try:
+        password = request_parts[1]
+    except:
+        return BAD_REQUEST
+
     user.set_password(password)
-    login = user.authenticate()
-    if login:
+    user.authenticate()
+    if user.authorized:
         response = 'Login Successfull'
     else:
         response = 'Login Failed'
@@ -71,15 +83,16 @@ def handle_list(request):
         path = request_parts[1]
     else:
         path = ''
-    command = f'ls {path} -l'
-    response = subprocess.run(shelx.split(command))
+    command = f'ls {path} -ltrh'
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
     return response
 
 def handle_retr(request, client_host):
     request_parts = request.strip().split()
-    path = subprocess.run(shelx.split('pwd'))
-    file_name = path + equest_parts[1] 
-    port = 20 
+    command = 'pwd'
+    path = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
+    file_name = path + request_parts[1] 
+    port = 8020 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((client_host, port))
@@ -92,7 +105,7 @@ def handle_retr(request, client_host):
             data = f.read(1024)
 
     s.close()
-    rosponse = 'File Sent'
+    response = 'File Sent'
     return response
 
 def handle_stor(request, client):
@@ -100,40 +113,59 @@ def handle_stor(request, client):
 
 def handle_mkd(request):
     request_parts = request.strip().split()
-    path = request_parts[1]
+    try:
+        path = request_parts[1]
+    except:
+        return BAD_REQUEST
+
     command = 'mkdir ' + path 
-    response = subprocess.run(shlex.split(command))
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
+    response = f'Directory cheeze "{path}" created' if not response else response
     return response 
 
 def handle_rmd(request):  
     request_parts = request.strip().split()
-    dir = request_parts[1]
-    command = 'rmdir ' + dir
-    response = subprocess.run(shlex.split(command))
+    try:
+        path = request_parts[1]
+    except:
+        return BAD_REQUEST
+
+    command = 'rmdir ' + path
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
+    response = f'Directory cheeze "{path}" created' if not response else response
     return response 
 
 def handle_pwd(request):
     command = 'pwd'
-    response = subprocess.run(shlex.split(command))
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
     return response 
 
 def handle_cwd(request):
     request_parts = request.strip().split()
-    dir = request_parts[1]
-    command = 'cd ' + dir
-    response = subprocess.run(shlex.split(command))
+    try:
+        path = request_parts[1]
+    except:
+        return BAD_REQUEST
+
+    command = 'cd ' + path
+    print(command)
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
     return response 
 
 def handle_dele(request):
     request_parts = request.strip().split()
-    name = request_parts[1]
+    try:
+        name = request_parts[1]
+    except:
+        return BAD_REQUEST
+
     command = 'rm -f' + name
-    response = subprocess.run(shlex.split(command))
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
     return response 
 
 def handle_cdup(request):
     command = 'cd ..'
-    response = subprocess.run(shlex.split(command))
+    response = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode()
     return response 
 
 def handle_quit(request, user):
@@ -144,7 +176,7 @@ def handle_quit(request, user):
 
 def main():
     host = "localhost"
-    port = 21 
+    port = 8021 
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
@@ -158,32 +190,35 @@ def main():
         client_host = socket.gethostbyaddr(client_address[0])[0]
         request = client_socket.recv(1024).decode()
 
-        if "USER" in request.to_upper():
+        if "USER" in request.upper():
             response = handle_user(request, user)
-        elif "PASS" in request.to_upper():
+        elif "PASS" in request.upper():
             response = handle_pass(request, user)
-        elif "LIST" in request.to_upper():
-            response = handle_list(request, client_host)
-        elif "RETR" in request.to_upper():
-            response = handle_retr(request, client_host)
-        elif "STOR" in request.to_upper():
-            response = handle_stor(request)
-        elif "MKD" in request.to_upper():
-            response = handle_mkd(request)
-        elif "RMD" in request.to_upper():
-            response = handle_rmd(request)
-        elif "PWD" in request.to_upper():
-            response = handle_pwd(request)
-        elif "CWD" in request.to_upper():
-            response = handle_cwd(request)
-        elif "DELE" in request.to_upper():
-            response = handle_dele(request)
-        elif "CDUP" in request.to_upper():
-            response = handle_cdup(request)
-        elif "QUIT" in request.to_upper():
-            response = handle_quit(request)
+        elif user.authorized:
+            if "LIST" in request.upper():
+                response = handle_list(request)
+            elif "RETR" in request.upper():
+                response = handle_retr(request, client_host)
+            elif "STOR" in request.upper():
+                response = handle_stor(request, client_host)
+            elif "MKD" in request.upper():
+                response = handle_mkd(request)
+            elif "RMD" in request.upper():
+                response = handle_rmd(request)
+            elif "PWD" in request.upper():
+                response = handle_pwd(request)
+            elif "CWD" in request.upper():
+                response = handle_cwd(request)
+            elif "DELE" in request.upper():
+                response = handle_dele(request)
+            elif "CDUP" in request.upper():
+                response = handle_cdup(request)
+            elif "QUIT" in request.upper():
+                response = handle_quit(request)
+            else:
+                response = BAD_REQUEST
         else:
-            response = "400 Bad Request"
+            response = "Login First!"
 
         client_socket.sendall(response.encode())
         client_socket.close()
